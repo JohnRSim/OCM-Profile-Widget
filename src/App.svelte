300c4bcd8b86ea582660e3cdb387fad09c69996e
John Sim <jsim@fishbowlsolutions.com>
2022-09-25 10:34:27 +0100

    add functionality too get/set prps load in profile image

--- src/App.svelte
+++ src/App.svelte
@@ -15,13 +15,17 @@
 	
 	//api
 	import Auth from './lib/api/auth.js';
+	import OCERemoteJSON from './lib/api/oce_remoteJSON.js';
 	import OCESocial from './lib/api/oce_social.js';
 
 	//stores
 	import { user as sUser } from './stores/user.js';
+	import { people as sPeople } from './stores/people.js';
+    import { each, prop_dev } from 'svelte/internal';
 
 	const social = new OCESocial();
 	const IDCS = new Auth();
+	const OSN = new OCERemoteJSON();
 
 	//globals
 	let isMounted = false;
@@ -39,6 +43,8 @@
 
 	onMount(() => {
 		isMounted = true;
+		//login get profile info
+		login();
 	});
 
 	/**
@@ -73,8 +79,20 @@
 				
 				//set sessionID
 				sessionID = ($sUser.useSessionRequests) ? sessionID : connection.apiRandomID;
+
 				sUser.updateConnectionInfo(connection.languageLocale, connection.apiVersion, sessionID);
-				//sUser.updateOCEVal('id',connection.id);
+				//sUser.updateOCEVal('id',connection.id);]
+				
+				//get Profile pic
+				//this image is too scaled down god fr small profile images so use alternative req below
+				//const [promiseProfilePic, abortProfilePic] = OSN.getMyProfilePic($sUser.session.oce, $sUser.sessionID);
+				const [promiseProfilePic, abortProfilePic] = OSN.getProfilePictureDataUri([connection.user.id], $sUser.session.oce, $sUser.sessionID);
+				
+				//store data
+				promiseProfilePic.then((profilePics) => {
+					console.log('[getMyProfilePic]',profilePics);
+					sPeople.updateProfileImg($sUser.profile.oce.id, profilePics[0].returnValue);
+				});
 
 				//store user profile info
 				sUser.updateProfileInfo(connection.user, 'oce');
@@ -84,11 +102,98 @@
 				flip = true;
 
 
+				//get User Stats 
+				const [promiseStats, abortStats] = social.getUserStats(connection.user.id, $sUser.session.oce);
+
+				promiseStats.then((res) => {
+					console.log('[getUserStats]',res);
+					const stats = {
+						followers: 0,
+						following: 0,
+						newConversations: 0,
+					};
+					res.items.forEach((v, i) => {
+						if (v.name === 'FOLLOWERS_COUNT') {
+							stats.followers = v.count;
+						}
+						if (v.name === 'FOLLOWING_COUNT') {
+							stats.following = v.count;
+						}
+						if (v.name === 'NEW_CONVERSATIONS_COUNT') {
+							stats.newConversations = v.count;
+						}
+						
+					});
+					//update stats
+					sUser.updateStats(stats);
+				});
+
+				//get all custom user properties
+				const [promiseProps, abortProps] = social.getAllUserProperties(connection.user.id, $sUser.session.oce);
+				
+				promiseProps.then((allUserProps) => {
+					console.log('[OCE][Profile][getAllUserProperties]',allUserProps);
+					
+					//check if user has custom props
+					if (allUserProps.items.some(prop => prop.name === 'Social_Enabled')) {
+						//update props
+						sUser.updateProps(allUserProps);
+					//create custom props
+					} else {
+						//make sure to use string - boolean didn't work or error just no respone
+						const userProps = [{
+							name: 'Social_email',
+							value: ''
+						},{
+							name: 'Social_facebook',
+							value: ''
+						},{
+							name: 'Social_linkedIn',
+							value: ''
+						},{
+							name: 'Social_twitter',
+							value: ''
+						},{
+							name: 'Social_web',
+							value: ''
+						},{
+							name: 'Social_Enabled',
+							value: 'true'
+						}];
+
+						userProps.forEach((prop, i) => {
+							sUser.updateProps(prop);
+							createUserProperty(prop,connection.user.id);
+						});
+					}
+				});
 			});
 		}).catch((err) => {
 			console.error('IDCS OCE Auth Error: ', err);
 			loginError = true;
 		});
+	}
+	
+	/**
+	 * createUserProperty
+	 * check if 'enter' key and then attempt to login
+	 **/
+	 function createUserProperty(prop, userID) {
+		const [promise, abort] = social.createUserProperty(userID, prop, $sUser.session.oce, $sUser.sessionID);
+
+		promise.then((createdProp) => {
+			console.log('[OCE][Profile][CreatedProp]',prop);
+		});
+	}
+
+	/**
+	 * getProfileImage
+	 * alternative example not used.
+	*/
+	function getProfilePic(userID) {
+		console.log('[getProfilePic]',userID);
+		const profilePicture = social.getProfilePic(userID,$sUser.session.oce);
+		//console.log('[getProfilePic][profilePicture]',profilePicture);
 	}
 
 	/**
@@ -114,29 +219,85 @@
 		};
 	}
 
+
+	/**
+	 * socialIco
+	*/
+	function socialIco(name) {
+		let img = '';
+		switch(name) {
+			case 'Social_email':
+				img = icoEmail;
+			break;
+			case 'Social_facebook':
+				img = icoFb;
+			break;
+			case 'Social_linkedIn':
+				img = icoLinkedin;
+			break;
+			case 'Social_twitter':
+				img = icoTwitter;
+			break;
+			case 'Social_web':
+				img = icoWeb;
+			break;
+		}
+		return img;
+	}
 </script>
 
 
 <!-- Wrapper -->
 <section class="bitmapbytes-profileWidget">
+	<!-- Widget -->
 	<article>
+		<!-- Container -->
 		<figure>
-			<img src="#" alt="Profile Image" />
+			<!-- Profile Image -->
+			{#if ($sPeople) && ($sPeople.profilePhoto) && ($sPeople.profilePhoto[72004])}
+				<img src="{$sPeople.profilePhoto[72004].img}" alt="Profile Image" />
+			{:else}
+				<img src="" alt="Profile Image" />
+			{/if}
+			<!-- xProfile Image -->
+
+			<!-- Copy -->
 			<figcaption>
+				<!-- User Info Panel -->
+				{#if (($sUser) && ($sUser.profile) && ($sUser.profile.oce)) }
 				<div class="userInfo">
-					<h2>JS Nashorn</h2>
-					<h3>Experimental Engineer</h3>
+					<!-- Display Name -->
+					{#if ($sUser.profile.oce.displayName)}
+						<h2>{$sUser.profile.oce.displayName}</h2>
+					{/if}
+					<!-- xDisplay Name -->
+
+					<!-- Title -->
+					{#if ($sUser.profile.oce.title)}
+						<h3>{$sUser.profile.oce.title}</h3>
+					{/if}
+					<!-- xTitle -->
 				</div>
+				{/if}
+				<!-- xUser Info Panel -->
+
+				<!-- User Social Links Panel -->
 				<nav class="socialLinks">
-					<a href="#" title="email"><img src="{icoEmail}" alt="email" /></a>
-					<a href="#" title="Facebook"><img src="{icoFb}" alt="Facebook" /></a>
-					<a href="#" title="LinkedIn"><img src="{icoLinkedin}" alt="LinkedIn" /></a>
-					<a href="#" title="Twitter"><img src="{icoTwitter}" alt="Twitter" /></a>
-					<a href="#" title="Website"><img src="{icoWeb}" alt="Website" /></a>
+					{#if (($sUser) && ($sUser.props) && ($sUser.props.items))}
+						{#each $sUser.props.items as prop}
+							{#if ((prop.name) && (prop.name.startsWith('Social_') && (prop.name !== 'Social_Enabled') && (prop.value.length > 0)))}
+								<a href="{prop.value}" title="{prop.name.replace(/Social_/g,'')}"><img src="{socialIco(prop.name)}" alt="{prop.name.replace(/Social_/g,'')}" /></a>
+							{/if}
+						{/each}
+					{/if}
 				</nav>
+				<!-- xUser Social Links Panel -->
 			</figcaption>
+			<!-- xCopy -->
 		</figure>
+		<!-- xContainer -->
 	</article>
+	<!-- xWidget -->
 </section>
 <!-- xWrapper -->
 
